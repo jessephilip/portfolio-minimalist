78ec2883364fb096dc5fe2fb8cc60e146d080bd7
jessephilip <jessephilip@gmail.com>
2017-01-15 19:58:55 -0600

    portfolio up and running. moving on to contact page

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -50,24 +50,40 @@ function loadAboutMe() {
     var aboutMeSection = document.getElementById("aboutMeSection");
     var intro = document.getElementById("intro");
     intro.className = "fadeOut";
+	portfolioSection.className = "fadeOut";
+	contactSection.className = "fadeOut";
     aboutMeSection.style.display = "initial";
     aboutMeSection.className = "fadeIn";
+	portfolioSection.style.zIndex = 0;
+	contactSection.style.zIndex = 0;
+	aboutMeSection.style.zIndex = 1;
+
 }
 
 function loadPortfolio() {
     var portfolioSection = document.getElementById("portfolioSection");
     var intro = document.getElementById("intro");
     intro.className = "fadeOut";
+	aboutMeSection.className = "fadeOut";
+	contactSection.className = "fadeOut";
     portfolioSection.style.display = "initial";
     portfolioSection.className = "fadeIn";
+	portfolioSection.style.zIndex = 1;
+	contactSection.style.zIndex = 0;
+	aboutMeSection.style.zIndex = 0;
 }
 
 function loadContact() {
     var contactSection = document.getElementById("contactSection");
     var intro = document.getElementById("intro");
     intro.className = "fadeOut";
+	portfolioSection.className = "fadeOut";
+	aboutMeSection.className = "fadeOut";
     contactSection.style.display = "initial";
     contactSection.className = "fadeIn";
+	portfolioSection.style.zIndex = 0;
+	contactSection.style.zIndex = 1;
+	aboutMeSection.style.zIndex = 0;
 }
 
 // this function replaces the black and white profile picture with the full color one
@@ -83,7 +99,7 @@ function blackAndWhiteify() {
 
 var portfolioArray = document.getElementsByClassName("portfolioPic")
 for (var i = 0; i < portfolioArray.length; i++) {
-	portfolioArray[i].style.cursor = "pointer";
+    portfolioArray[i].style.cursor = "pointer";
 }
 
 // click listener
@@ -93,33 +109,88 @@ $(".portfolioPic").mouseout(pictureSwitch);
 // this function will change the portfolio picture from black and white to color
 function pictureSwitch() {
 
-	// variable to determine which portfolio div is hovered over
+    // variable to determine which portfolio div is hovered over
     let check = $(this).data("color");
 
-	// split check once and join the created array to get the title of the portfolio project
-	let id = check.split("_", 1);
-	id = id.join();
+    // split check once and join the created array to get the title of the portfolio project
+    let id = check.split("_", 1);
+    id = id.join();
 
-	// conditional to change the color of the project title. if red make right and vice versa.
-	if ($(this).children("h4").css("color") === "rgb(255, 0, 0)") $(this).children("h4").css("color", "white");
-	else $(this).children("h4").css("color", "red");
+    // conditional to change the color of the project title. if red make right and vice versa.
+    if ($(this).children("h4").css("color") === "rgb(128, 128, 128)") {
+        $(this).children("h4").css("color", "white");
+    } else
+        $(this).children("h4").css("color", "grey");
 
-	// variable to hold image source tag
+    // variable to hold image source tag
     let imgSrc;
 
-	// variable for the preface of the image source tag
+    // variable for the preface of the image source tag
     let loc = "assets/images/";
 
-	// variable pointing to the img element in the div
-	let imgTag = $(this).children("img");
+    // variable pointing to the img element in the div
+    let imgTag = $(this).children("img");
 
-	// if img src has "_bw" change the img src to the color image and vice versa
+    // if img src has "_bw" change the img src to the color image and vice versa
     if (imgTag.attr("src").indexOf("_bw") === -1) {
         imgSrc = $(this).data("bw");
         imgTag.attr("src", loc + imgSrc);
+    } else {
+        imgSrc = $(this).data("color");
+        imgTag.attr("src", loc + imgSrc);
     }
-	else {
-		imgSrc = $(this).data("color");
-	    imgTag.attr("src", loc + imgSrc);
-	}
 }
+
+// clickfunction for clicking on a portfolio project picture. loads a sweetalert modal
+$(".portfolioPics").on("click", (e) => {
+    let loc = "assets/images/";
+    let target = e.target.alt;
+    let id = target.split(" ", 1);
+    id = id.join();
+    console.log(portfolio[id]);
+
+    $('#exampleModal').modal();
+    let titleLoc = $("#modalTitle");
+    titleLoc.text(portfolio[id].name);
+
+    let imgLoc = $("#modalImg");
+    imgLoc.attr("src", portfolio[id].photo);
+    imgLoc.attr("alt", portfolio[id].name + " color image.");
+
+    $("#modalList").html("");
+
+    // create an <li> element to add the technologies text
+    let techLi = $("<li>");
+
+    // prepare technologies array for modal by converting to string
+    let tech = portfolio[id].tech.join();
+    tech = tech.replace(/,/g, ", ");
+    techLi.text("Technologies: " + tech);
+
+    // append to the <ul> element
+    $("#modalList").append(techLi);
+
+    // conditional to see if npm packages are present
+    if (portfolio[id].hasOwnProperty("npm")) {
+        if (portfolio[id].npm.length > 0) {
+
+            // create an <li> element to add the npm text
+            let npmLi = $("<li>");
+
+            // manipulate the npm array to be presentable on the modal as a string
+            let npm = portfolio[id].npm.join();
+            npm = npm.replace(/,/g, ", ");
+            npmLi.text("NPM Packages: " + npm);
+
+            // append that element to the modal list
+            $("#modalList").append(npmLi);
+        }
+    }
+
+    $("#modalDesc").text(portfolio[id].description);
+
+    $("#modalGithub").attr("href", portfolio[id].github);
+
+    $("#modalHeroku").attr("href", portfolio[id].heroku);
+
+});
--- assets/javascript/projects.js
+++ assets/javascript/projects.js
@@ -1,61 +1,57 @@
 const portfolio = {
 	"hangman": {
 		"name": "Hangman",
+		"photo": 'assets/images/hangman_edited.jpg',
+		"heroku": "https://pure-mountain-27349.herokuapp.com/",
+		"github": "https://github.com/jessephilip/hangman",
 		"tech": ["HTML", "CSS", "Javascript", "Bootstrap", "Google Fonts", "Font Awesome", "Microsoft Paint"],
-		"description": 'Everyone knows Hangman, that childhood game of knowledge, skill, and luck. This is my version of that classic. This was one of the first class homeworks, and I did my best to make this assignment my own.
-
-		I imbued it with my sense of humor and tried to achieve a retro style. I went above the parameters of the assignment to "paint" my own graphics with Microsoft Paint. There are also YouTube videos scattered about, a scoring system, and three difficulty settings.
-
-		To this day, this is one of my favorite websites, and if you permit me to say so, I actually find it quite entertaining.'
+		"description": 'Everyone knows Hangman, that childhood game of knowledge, skill, and luck. This is my version of that classic. This was one of my first homework assignments with the UT Coding Bootcamp, and I did my best to make this assignment my own. I imbued it with my sense of humor and tried to achieve a retro style. I went above the parameters of the assignment to "paint" my own graphics with Microsoft Paint. There are also YouTube videos scattered about, a scoring system, and three difficulty settings. To this day, this is one of my favorite websites, and if you permit me to say so, I actually find it quite entertaining.'
 	},
 
 	"fetch": {
 		"name": "Fetch",
+		"photo": "assets/images/fetch_edited.jpg",
+		"heroku": "",
+		"github": "https://github.com/jessephilip/Pet-Tinder",
 		"tech": ["HTML", "CSS", "Javascript", "jQuery", "MaterializeCSS", "Google Fonts", "Font Awesome", "Alertify.js", "Google Sign-In", "Node.js", "NPM", "MySQL", "RegEx", "Validator"],
-		"npm": ["bcrypt", "body-parser", "express", "mysql", "nightmare", "sequelize", "chai", "mocha", "request"],
-		"description": 'Fetch is a Tinder-style solution to pet adoptions. It was designed to be a frontend website for adoption agencies to host their avaiable pets and aid in the pet adoption process.
-
-		This was a group project in which I was involved in its concept and implementation. I worked on both the user experience and backend code, having to edit HTML, CSS, and Javascript. I am particularly proud of my code that set up the Google "One Click" Sign-In experience.
-
-		I consider this produce to be in its Alpha production and have many plans for further improving it in the future.'
+		"npm": ["BCrypt", "Body-Parser", "Express", "MySQL", "Nightmare", "Sequelize", "Chai", "Mocha", "Request"],
+		"description": 'Fetch is a Tinder-style solution to pet adoptions. It was designed to be a frontend website for adoption agencies to host their avaiable pets and aid in the pet adoption process. This was a group project in which I was involved in its concept and implementation. I worked on both the user experience and backend code, having to edit HTML, CSS, and Javascript. I am particularly proud of my code that set up the Google "One Click" Sign-In experience. I consider this produce to be in its Alpha production and have many plans for further improving it in the future.'
 	},
 
 	"mtg": {
 		"name": "MTG the Console",
+		"photo": "assets/images/mtg_console_edited.jpg",
+		"heroku": "https://tranquil-cliffs-20102.herokuapp.com/",
+		"github": "https://github.com/jessephilip/mtg-firebase",
 		"tech": ["HTML", "CSS", "Javascript", "jQuery", "MaterializeCSS", "Firebase", "Google Sign-In", "Node.js", "NPM"],
-		"npm": ["body-parser", "express", "firebase", "google-auth-library"],
-		"description": 'MTG the Console is a pet project of mine that I am creating for my local play group. At its heart, it is a tool to keep track of each player\'s life total. In reality, it does so much more.
-
-		Utilizing Firebase.auth() and Google Sign-In, players can join games and see the progress of the game no matter where they are. I plan to provide additional tools such as storyboarding, deck building, and custom point systems.
-
-		MTG the Console is, at heart, my training tool, therefore it has seen many iterations and rewrites as my knowledge of programming has progressed. The original MTG the Console was actually an Android app written in Java and XML using Android Studio. During my education with the UT Coding Bootcamp, it transformed into a simple HTML site with limited Javascript, to using an SQL database, to the current version which utilizes Firebase for authentication and databasing. I plan to put it back on the phone by reworking it for React-Native.'
+		"npm": ["Body-Parser", "Express", "Firebase", "Google-Auth-Library"],
+		"description": 'MTG the Console is a pet project of mine that I am creating for my local play group. At its heart, it is a tool to keep track of each player\'s life total. In reality, it does so much more. Utilizing Firebase.auth() and Google Sign-In, players can join games and see the progress of the game no matter where they are. \r I plan to provide additional tools such as storyboarding, deck building, and custom point systems.	MTG the Console is, at heart, my training tool, therefore it has seen many iterations and rewrites as my knowledge of programming has progressed. The original MTG the Console was actually an Android app written in Java and XML using Android Studio. During my education with the UT Coding Bootcamp, it transformed into a simple HTML site with limited Javascript, to using an SQL database, to the current version which utilizes Firebase for authentication and databasing. I plan to put it back on the phone by reworking it for React-Native.'
 	},
 
 	"ranger": {
 		"name": "Ranger",
+		"photo": "assets/images/ranger_edited.jpg",
+		"heroku": "",
+		"github": "https://github.com/jessephilip/Hiking-App",
 		"tech": ["HTML", "CSS", "Javascript", "jQuery", "MaterializeCSS", "Google Fonts", "Alertify.js", "Firebase", "Google Places API", "Open Trails API", "Weather Underground API"],
-		"description": 'Ranger is a weather and hiking website designed to assist someone in planning an outdoor adventure. Utilizing HTML5\'s geolocation, Ranger determines where the user is currently located and provides the user with the weather, a weather forecast, and nearby trails and parks. Ranger also sports a small social-networking feature that allows users to update photographs of parks they have visited.
-
-		Ranger was my first group project with the UT Coding Bootcamp. I was involved in the conception, design, and implementation of the website. I helped the team write up the HTML and CSS, but I mainly worked in the Javascript for this project. I am most proud of my code that used the Open Trails API to populate the Google map with individual map markers.
-
-		When I get the time, I plan to revisit Ranger and detach it from the Open Trails API, so that it becomes a strictly Google Places website.'
+		"description": 'Ranger is a weather and hiking website designed to assist someone in planning an outdoor adventure. Utilizing HTML5\'s geolocation, Ranger determines where the user is currently located and provides the user with the weather, a weather forecast, and nearby trails and parks. Ranger also sports a small social-networking feature that allows users to update photographs of parks they have visited. Ranger was my first group project with the UT Coding Bootcamp. I was involved in the conception, design, and implementation of the website. I helped the team write up the HTML and CSS, but I mainly worked in the Javascript for this project. I am most proud of my code that used the Open Trails API to populate the Google map with individual map markers. \n When I get the time, I plan to revisit Ranger and detach it from the Open Trails API, so that it becomes a strictly Google Places website.'
 	},
 
 	"wedding": {
-		"name": "Happily Ever Matherne Website",
+		"name": "Happily Ever Matherne",
+		"photo": "assets/images/wedding_edited.jpg",
+		"heroku": "https://blooming-refuge-83262.herokuapp.com/",
+		"github": "https://github.com/jessephilip/wedding-website",
 		"tech": ["HTML", "CSS", "Javascript", "jQuery", "Bootstrap", "Google Fonts", "Font Awesome", "Hover.CSS", "jCarousel"],
-		"description": 'I\'m married!!! Go me!!!
-
-		On December 17, 2016, I married the woman of my dreams.
-
-		Yes, I know this isn\'t proper portfolio material, but this website is on here for one purpose: to show you how driven I am. And that I can multi-task (so make that two purposes). Coding Bootcamps are time-consuming, strenuous, races to fill a student\'s head with as much knowledge and experience within a short period of time. I was in school full time and studying full time. And in the heart of that, I was planning and preparing a wedding! (as well as juggling my own side projects. We also adopted another dog!).
-
-		Please be impressed. I worked hard to do so, and it would make the Mrs. happy.'
+		"description": 'I\'m married!!! Go me!!! On December 17, 2016, I married the woman of my dreams. \n Yes, I know this isn\'t proper portfolio material, but this website is on here for one purpose: to show you how driven I am. And that I can multi-task (so make that two purposes). Coding Bootcamps are time-consuming, strenuous, races to fill a student\'s head with as much knowledge and experience within a short period of time. I was in school full time and studying full time. And in the heart of that, I was planning and preparing a wedding! (as well as juggling my own side projects. We also adopted another dog!).	Please be impressed. I worked hard to do so, and it would make the Mrs. happy.'
 
 	},
 
 	"giphy": {
 		"name": "Giphy in a Jiffy",
+		"photo": "assets/images/giphy_edited.jpg",
+		"heroku": "https://shielded-earth-15957.herokuapp.com/",
+		"github": "https://github.com/jessephilip/giphy",
 		"tech": ["HTML", "CSS", "Javascript", "jQuery", "Bootstrap", "Google Fonts"],
 		"description": 'Giphy in a Jiffy is a simple application which uses https://api.giphy.com/ to generate giphys based on the user\'s input. It was one of my first forays into dynamically generated content.'
 	}
